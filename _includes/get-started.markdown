49d5345909864ea0587cbc1f6a6f9217c2a271e7
slemesle <slemesle@xebia.fr>
2015-01-26 08:34:17 +0100

    Import Jekyll site under development to git

---
+++ _includes/get-started.markdown
@@ -0,0 +1,21 @@
+
+h1. Set Up
+
+First add Selma to your dependencies.
+
+```xml
+        <dependency>
+            <groupId>fr.xebia.extras</groupId>
+            <artifactId>selma-processor</artifactId>
+            <version>${project.version}</version>
+            <scope>provided</scope>
+        </dependency>
+
+        <dependency>
+            <groupId>fr.xebia.extras</groupId>
+            <artifactId>selma</artifactId>
+            <version>${project.version}</version>
+        </dependency>
+```
+
+
--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+(function ($) {
+    'use strict';
 
+
+    $(document).ready(function() {
+// Sticky Header on Scroll
+        var menu = $('#top-header');
+        var origOffsetY = menu.offset().top;
+
+        function scroll() {
+            if ($(window).scrollTop() >= origOffsetY) {
+                menu.addClass('sticky');
+            } else {
+                menu.removeClass('sticky');
+            }
+        }
+
+        document.onscroll = scroll;
+
+        $('a').smoothScroll();
+
+    });
+
+})(jQuery);
